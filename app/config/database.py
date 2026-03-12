22bb94c34902552efa1dfa10002cb4d291c6901a
Shamil Lachenilaev <lachen1305@gmail.com>
2026-03-12 08:54:50 +0300

    refactor: add SQLAlchemyConfig, async_url property

--- app/config/database.py
+++ app/config/database.py
@@ -1,10 +1,30 @@
 from pydantic import BaseModel, SecretStr
+from sqlalchemy import URL
+
+
+class SQLAlchemyConfig(BaseModel):
+    pool_size: int = 20
+    max_overflow: int = 5
+    echo: bool = False
 
 
 class DatabaseConfig(BaseModel):
-    database: str
-    user: str
+    username: str
     password: SecretStr
     host: str
     port: int
-    echo: bool = False
+    database: str
+
+    sqla: SQLAlchemyConfig = SQLAlchemyConfig()
+
+    @property
+    def asyncpg_url(self) -> URL:
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            username=self.username,
+            password=self.password.get_secret_value(),
+            host=self.host,
+            port=self.port,
+            database=self.database,
+
+        )
