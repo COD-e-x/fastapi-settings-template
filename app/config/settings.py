710876580d0d895821a2875ab7e55feb89cfe952
Shamil Lachenilaev <lachen1305@gmail.com>
2026-03-12 06:40:37 +0300

    feat: add yaml config source

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
+    PydanticBaseSettingsSource,
+    YamlConfigSettingsSource,
 )
 
 from app.config.app import AppConfig
@@ -12,6 +14,7 @@ from app.config.logging import LoggingConfig
 
 CONFIG_DIR = Path(__file__).resolve().parent
 ENVS_DIR = CONFIG_DIR / "envs"
+YAML_DIR = CONFIG_DIR / "yaml"
 
 
 class Settings(BaseSettings):
@@ -23,11 +26,35 @@ class Settings(BaseSettings):
             ENVS_DIR / ".env.example",
             ENVS_DIR / ".env",
         ),
+        yaml_config_section="fst",
+        yaml_file=(
+            YAML_DIR / "default.yaml",
+            YAML_DIR / "local.yaml",
+        )
     )
     app: AppConfig
     logging: LoggingConfig
     database: DatabaseConfig
     http: HttpConfig = HttpConfig()
+
+    @classmethod
+    def settings_customise_sources(
+            cls,
+            settings_cls: type[BaseSettings],
+            init_settings: PydanticBaseSettingsSource,
+            env_settings: PydanticBaseSettingsSource,
+            dotenv_settings: PydanticBaseSettingsSource,
+            file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            YamlConfigSettingsSource(
+                settings_cls,
+                deep_merge=True,
+            ),
+        )
 
 
 settings = Settings()
